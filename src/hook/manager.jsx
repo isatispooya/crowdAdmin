0fb795d3258a4ef4db0dafaf679f6e0c91064c5a
mhmdalisadat <150137653+mhmdalisadat@users.noreply.github.com>
2024-09-01 07:43:54 +0330

    12345787

--- src/hook/manager.jsx
+++ src/hook/manager.jsx
@@ -1,11 +1,10 @@
-/* eslint-disable consistent-return */
 import axios from 'axios';
 import api from 'src/api/apiClient';
 import { getCookie } from 'src/api/cookie';
 import { OnRun } from 'src/api/OnRun';
 
 export const fetchManager = async (id) => {
-
+  try {
     const access = await getCookie('access');
     
     const response = await api.get(`/api/manager/admin/${id}/`, {
@@ -14,27 +13,29 @@ export const fetchManager = async (id) => {
         'Content-Type': 'application/json',
       },
     });
+    
     return response.data;
+  } catch (error) {
+    console.error('Error fetching manager data:', error);
+    throw new Error('Failed to fetch manager data.');
+  }
 };
 
-
-
-
-
 export const sendManager = async (id, data) => {
-  const access = await getCookie('access');
-  const url = `${OnRun}/api/manager/admin/${id}/`;
-  const response = await axios.post(
-    url,
-    data  ,
-    {
+  try {
+    const access = await getCookie('access');
+    const url = `${OnRun}/api/manager/admin/${id}/`;
+
+    const response = await axios.post(url, data, {
       headers: {
         Authorization: `Bearer ${access}`,
         'Content-Type': 'application/json',
-      }
-    }
-  );
+      },
+    });
 
-  return response.data;
+    return response.data;
+  } catch (error) {
+    console.error('Error sending manager data:', error);
+    throw new Error('Failed to send manager data.');
+  }
 };
-
