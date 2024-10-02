ae0db342228206c38d57ccd4ed3ade3716244bd4
ZahraAmini <112477210+ZahraAmini1112@users.noreply.github.com>
2024-10-02 10:31:42 +0330

    ...

--- src/module/contract/feature/contentfeature.jsx
+++ src/module/contract/feature/contentfeature.jsx
@@ -8,25 +8,27 @@ import ContentInput from './contractInput';
 import SwitchContract from './switchInputs';
 import UsePostContract from '../services/usePostContract';
 
-const ContractFeature = ({ handelClick, setContractData, contractData }) => {
+const ContractFeature = ({ setContractData, contractData }) => {
   const { cartId } = UseCartId();
-  const { mutate, isLoading, IsError, isPending, error } = UsePostContract(cartId);
-
+  const { mutate } = UsePostContract(cartId);
 
   const postContractData = () => {
-    mutate(contractData)
-  }
+    mutate(contractData);
+  };
   return (
     <Grid spacing={3}>
       <ContentInput contractData={contractData} setContractData={setContractData} />
       <SwitchContract contractData={contractData} setContractData={setContractData} />
-      <SubmitButton contractData={contractData} onClick={postContractData} postContractData={postContractData} />
+      <SubmitButton
+        contractData={contractData}
+        onClick={postContractData}
+        postContractData={postContractData}
+      />
     </Grid>
   );
 };
 
 ContractFeature.propTypes = {
-  handelClick: PropTypes.func,
   contractData: PropTypes.object.isRequired,
   setContractData: PropTypes.func.isRequired,
 };
