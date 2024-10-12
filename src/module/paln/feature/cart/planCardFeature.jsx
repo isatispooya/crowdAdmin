f011b687d07afd678fccfaa5b9ba7cddaa1a9d78
ZahraAmini <112477210+ZahraAmini1112@users.noreply.github.com>
2024-10-12 09:53:02 +0330

    ..

--- src/module/paln/feature/cart/planCardFeature.jsx
+++ src/module/paln/feature/cart/planCardFeature.jsx
@@ -11,7 +11,6 @@ import useUpdatePlan from '../../service/planCard/useUpdatePlan';
 const PlanTableFeature = () => {
   const [planData, setPlanData] = useState([]);
   const navigate = useNavigate();
-
   const [isRotating, setIsRotating] = useState(false);
 
   const { data: plans, isLoading, isError } = useGetPlans();
@@ -28,6 +27,23 @@ const PlanTableFeature = () => {
   const formatNumber = (value) => {
     if (value == null) return '';
     return value.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+  };
+
+  const getStatusTitle = (status) => {
+    switch (status) {
+      case '1':
+        return 'شروع شده';
+      case '2':
+        return 'جمع آوری';
+      case '3':
+        return 'تمدید شده';
+      case '4':
+        return 'سررسید ناموفق';
+      case '5':
+        return  'تکمیل';
+      default:
+        return 'نامشخص';
+    }
   };
 
   useEffect(() => {
@@ -69,85 +85,123 @@ const PlanTableFeature = () => {
       {!isLoading && planData.length === 0 && <Typography>هیچ طرحی وجود ندارد</Typography>}
 
       <Grid container spacing={2}>
-        {!isLoading &&
-          planData.length > 0 &&
-          planData.map((plan) => (
-            <Grid item xs={12} sm={4} md={3} key={plan.plan.trace_code}>
-              <Card
-                sx={{
-                  display: 'flex',
-                  flexDirection: 'column',
-                  justifyContent: 'space-between',
-                  height: '100%',
-                  padding: '20px',
-                  cursor: 'pointer',
-                  boxShadow: '0 4px 15px rgba(0, 0, 0, 0.2)',
-                  transition: 'transform 0.2s, box-shadow 0.2s',
-                  '&:hover': {
-                    transform: 'scale(1.05)',
-                    boxShadow: '0 8px 25px rgba(0, 0, 0, 0.3)',
-                  },
-                  borderRadius: '15px',
-                }}
-                onClick={() => handleCardClick(plan.plan.trace_code)}
-              >
-                <CardContent
+        <Grid container spacing={2}>
+          {!isLoading &&
+            planData.length > 0 &&
+            planData.map((plan) => (
+              <Grid item xs={12} sm={4} md={3} key={plan.plan.trace_code}>
+                <Card
                   sx={{
-                    flexGrow: 1,
                     display: 'flex',
                     flexDirection: 'column',
-                    justifyContent: 'center',
-                    alignItems: 'center',
-                    textAlign: 'center',
+                    justifyContent: 'space-between',
+                    height: '100%',
+                    padding: '20px',
+                    position: 'relative', 
+                    boxShadow:
+                      plan.information_complete.status_second === '5'
+                        ? 'none'
+                        : '0 4px 15px rgba(0, 0, 0, 0.2)',
+                    transition: 'transform 0.2s, box-shadow 0.2s',
+                    backgroundColor:
+                      plan.information_complete.status_second === '5' ? '#d0d3d4' : 'white',
+                    '&:hover': {
+                      transform:
+                        plan.information_complete.status_second === '5' ? 'none' : 'scale(1.05)',
+                      boxShadow:
+                        plan.information_complete.status_second === '5'
+                          ? 'none'
+                          : '0 8px 25px rgba(0, 0, 0, 0.3)',
+                    },
+                    borderRadius: '15px',
+                    opacity: plan.information_complete.status_second === '5' ? 0.8 : 1,
                   }}
+                  onClick={() =>
+                    plan.information_complete.status_second !== '5' &&
+                    handleCardClick(plan.plan.trace_code)
+                  }
                 >
-                  <Typography variant="h6" component="div" sx={{ fontWeight: 'bold', mb: 1 }}>
-                    {plan.plan.persoan_approved_symbol || 'بدون نام'}
-                  </Typography>
-                  <Typography variant="p" component="div" sx={{  mb: 1,color:'#808b96',fontSize:'17px' }}>
-                    {plan.plan.persian_name || 'بدون نام'}
-                  </Typography>
-                  <Typography variant="body2" sx={{ mb: 1 }}>
-                    مبلغ تعیین شده:{' '}
-                    {plan.plan.total_price
-                      ? `${formatNumber(plan.plan.total_price)} ریال`
-                      : 'نامشخص'}
-                  </Typography>
-                  <Typography variant="body2" sx={{ mb: 1 }}>
-                    تعداد واحدها:{' '}
-                    {plan.plan.company_unit_counts
-                      ? formatNumber(plan.plan.company_unit_counts)
-                      : 'نامشخص'}
-                  </Typography>
-                  <Typography variant="body2" sx={{ mb: 1 }}>
-                    قیمت واحد:{' '}
-                    {plan.plan.unit_price ? `${formatNumber(plan.plan.unit_price)} ریال` : 'نامشخص'}
+                  <Typography
+                    variant="p"
+                    sx={{
+                      position: 'absolute',
+                      top: '10px',
+                      left: '10px',
+                      color: '#fff',
+                      backgroundColor: 'rgba(0, 0, 0, 0.3)', 
+                      padding: '5px',
+                      borderRadius: '5px',
+                      zIndex: 1, 
+                    }}
+                  >
+                    {getStatusTitle(plan.information_complete.status_second)}
                   </Typography>
 
-                  <Typography variant="body2" sx={{ mb: 1 }}>
-                    صنعت: {plan.plan.industry_group_description || 'نامشخص'}
-                  </Typography>
+                  <CardContent
+                    sx={{
+                      flexGrow: 1,
+                      display: 'flex',
+                      flexDirection: 'column',
+                      justifyContent: 'center',
+                      alignItems: 'center',
+                      textAlign: 'center',
+                    }}
+                  >
+                    <Typography variant="h6" component="div" sx={{ fontWeight: 'bold', mb: 1 }}>
+                      {plan.plan.persoan_approved_symbol || 'بدون نام'}
+                    </Typography>
+                    <Typography variant="h6" component="div" sx={{ fontWeight: 'bold', mb: 1 }}>
+                      {getStatusTitle(plan.information_complete.status_second)}
+                    </Typography>
+                    <Typography
+                      variant="p"
+                      component="div"
+                      sx={{ mb: 1, color: '#808b96', fontSize: '17px' }}
+                    >
+                      {plan.plan.persian_name || 'بدون نام'}
+                    </Typography>
+                    <Typography variant="body2" sx={{ mb: 1 }}>
+                      مبلغ تعیین شده:{' '}
+                      {plan.plan.total_price
+                        ? `${formatNumber(plan.plan.total_price)} ریال`
+                        : 'نامشخص'}
+                    </Typography>
+                    <Typography variant="body2" sx={{ mb: 1 }}>
+                      تعداد واحدها:{' '}
+                      {plan.plan.company_unit_counts
+                        ? formatNumber(plan.plan.company_unit_counts)
+                        : 'نامشخص'}
+                    </Typography>
+                    <Typography variant="body2" sx={{ mb: 1 }}>
+                      قیمت واحد:{' '}
+                      {plan.plan.unit_price
+                        ? `${formatNumber(plan.plan.unit_price)} ریال`
+                        : 'نامشخص'}
+                    </Typography>
+                    <Typography variant="body2" sx={{ mb: 1 }}>
+                      صنعت: {plan.plan.industry_group_description || 'نامشخص'}
+                    </Typography>
+                    <Typography variant="body2" sx={{ mb: 1 }}>
+                      تاریخ:{' '}
+                      {plan.creation_date
+                        ? moment(plan.plan.creation_date).format('jYYYY/jMM/jDD')
+                        : 'نامشخص'}
+                    </Typography>
+                  </CardContent>
 
-                  <Typography variant="body2" sx={{ mb: 1 }}>
-                    تاریخ:{' '}
-                    {plan.creation_date
-                      ? moment(plan.plan.creation_date).format('jYYYY/jMM/jDD')
-                      : 'نامشخص'}
-                  </Typography>
-                </CardContent>
-
-                <Button
-                  fullWidth
-                  onClick={() => handleCardClick(plan.plan.trace_code)}
-                  variant="contained"
-                  color="primary"
-                  sx={{ textTransform: 'none', mt: 2 }}
-                >
-                  مشاهده
-                </Button>
-              </Card>
-            </Grid>
-          ))}
+                  <Button
+                    fullWidth
+                    onClick={() => handleCardClick(plan.plan.trace_code)}
+                    variant="contained"
+                    color="primary"
+                    sx={{ textTransform: 'none', mt: 2 }}
+                  >
+                    مشاهده
+                  </Button>
+                </Card>
+              </Grid>
+            ))}
+        </Grid>
       </Grid>
     </Box>
   );
